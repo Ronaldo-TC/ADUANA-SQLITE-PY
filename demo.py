8b5c6abb902a9c7a9b98b72490f38308153bfe1b
Ronaldo-TC <ronaldotarquicopa@gmail.com>
2024-12-10 16:37:19 -0400

    primer commit

---
+++ demo.py
@@ -0,0 +1,3 @@
+sk-proj-X3hELVAGBm4nK-_MbhYiR-yt2u0RuDXgA49-ahbxQSdZGMwomB - \
+    V7KJal8UzMO83eClgeJPW1AT3BlbkFJU40MdjuHeAnkB1 - \
+    TDH2kjCULgx_ukzvkK2Logq8sLq4qcaAg5lusYL9XAmD1jiA7HIpvyDCywA
